9d264aa776a1f4cf353947da3ba16ea67edfd269
Bram van der Kroef <bram@vanderkroef.net>
2011-08-30 12:51:29 -0500

    Added Writer for outputting Stylesheets as text.

---
+++ src/Writer.h
@@ -0,0 +1,22 @@
+#ifndef __Writer_h__
+#define __Writer_h__
+
+#include "Stylesheet.h"
+#include <string>
+#include <vector>
+using namespace std;
+
+class Writer {
+public:
+  Writer(ostream* out);
+  void writeStylesheet(Stylesheet* s);
+
+private:
+  ostream* out;
+  void writeAtRule(AtRule* rule);
+  void writeRuleset(Ruleset* ruleset);
+  void writeDeclaration(Declaration* declaration);
+  
+}
+  
+#endif
