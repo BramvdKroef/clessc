51a8c8ae0ba514b350ac86aad949701afc3771f3
Bram van der Kroef <bram@vanderkroef.net>
2011-08-31 08:32:59 -0500

    Added css spec to tokenizer comments.

--- src/Tokenizer.h
+++ src/Tokenizer.h
@@ -11,24 +11,87 @@ using namespace std;
 
 
 /**
- * Leest char's uit een implementatie van Reader en herkent de typen zoals
- * hieronder aangegeven. Er worden char's gelezen totdat het laastgelezen char
- * een ander type heeft dan de vorigen. Spaties worden niet gezien als token. In
- * een getal mag ook een punt voorkomen.
+ * Converts the input stream to CSS tokens according to the syntax spec at
+ * http://www.w3.org/TR/CSS2/syndata.html.
  *
- * Met getTokenType() kan het type van het token gelezen worden en met
- * getToken() kan een pointer naar het token verkregen worden.
+ * Excerpt:
  *
- * Elke keer als de Tokenizer een End Of Line(TT_EOL) token tegenkomt word
- * lineno met 1 verhoogd.
+ *   IDENT      	{ident}
+ *   ATKEYWORD  	@{ident}
+ *   STRING     	{string}
+ *   BAD_STRING 	{badstring}
+ *   BAD_URI    	{baduri}
+ *   BAD_COMMENT 	{badcomment}
+ *   HASH       	#{name}
+ *   NUMBER     	{num}
+ *   PERCENTAGE 	{num}%
+ *   DIMENSION  	{num}{ident}
+ *   URI        	url\({w}{string}{w}\)
+ *                      |url\({w}([!#$%&*-\[\]-~]|{nonascii}|{escape})*{w}\)
+ *   UNICODE-RANGE 	u\+[0-9a-f?]{1,6}(-[0-9a-f]{1,6})?
+ *   CDO        	<!--
+ *   CDC        	-->
+ *   :          	:
+ *   ;          	;
+ *   {          	\{
+ *   }          	\}
+ *   (          	\(
+ *   )          	\)
+ *   [          	\[
+ *   ]          	\]
+ *   S          	[ \t\r\n\f]+
+ *   COMMENT     	\/\*[^*]*\*+([^/*][^*]*\*+)*\/
+ *   FUNCTION   	{ident}\(
+ *   INCLUDES    	~=
+ *   DASHMATCH   	|=
+ *   DELIM      	any other character not matched by the above
+ *                      rules, and neither a single nor a double quote 
+ *
+ * Notes
+ *
+ * The CDO and CDC tokens are not supported as they are not expected in a
+ * css file.
+ *
+ * Instead of generating BAD_STRING, BAD_URI and BAD_COMMENT tokens
+ * the Tokenizer throws a ParseException. This may change in the future so the
+ * Parser can handle them.
+ *
+ * FUNCTION tokens are not generated. If an identifier is followed by a '(' the
+ * parser will handle it from there.
+ *
+ * The macros in curly braces ({}) above are defined as follows:
+ *   Macro      	Definition
+ *   ident      	[-]?{nmstart}{nmchar}*
+ *   name       	{nmchar}+
+ *   nmstart    	[_a-z]|{nonascii}|{escape}
+ *   nonascii   	[^\0-\237]
+ *   unicode    	\\[0-9a-f]{1,6}(\r\n|[ \n\r\t\f])?
+ *   escape     	{unicode}|\\[^\n\r\f0-9a-f]
+ *   nmchar     	[_a-z0-9-]|{nonascii}|{escape}
+ *   num        	[0-9]+|[0-9]*\.[0-9]+
+ *   string     	{string1}|{string2}
+ *   string1    	\"([^\n\r\f\\"]|\\{nl}|{escape})*\"
+ *   string2    	\'([^\n\r\f\\']|\\{nl}|{escape})*\'
+ *   badstring   	{badstring1}|{badstring2}
+ *   badstring1 	\"([^\n\r\f\\"]|\\{nl}|{escape})*\\?
+ *   badstring2 	\'([^\n\r\f\\']|\\{nl}|{escape})*\\?
+ *   badcomment 	{badcomment1}|{badcomment2}
+ *   badcomment1 	\/\*[^*]*\*+([^/*][^*]*\*+)*
+ *   badcomment2 	\/\*[^*]*(\*+[^/*][^*]*)*
+ *   baduri     	{baduri1}|{baduri2}|{baduri3}
+ *   baduri1    	url\({w}([!#$%&*-~]|{nonascii}|{escape})*{w}
+ *   baduri2    	url\({w}{string}{w}
+ *   baduri3    	url\({w}{badstring}
+ *   nl         	\n|\r\n|\r|\f
+ *   w          	[ \t\r\n\f]*
  */
 class Tokenizer {
 public:
-  enum tokenType{ATKEYWORD, HASH, STRING, NUMBER, PERCENTAGE, DIMENSION,
-                 IDENTIFIER, URL, WHITESPACE, EOS, UNICODE_RANGE, OTHER,
-                 DASHMATCH, DELIMITER, COLON, BRACKET_OPEN, BRACKET_CLOSED,
-                 PAREN_OPEN, PAREN_CLOSED, BRACE_OPEN, BRACE_CLOSED, INCLUDES,
-                 COMMENT}; 
+  enum tokenType{IDENTIFIER, ATKEYWORD, STRING, HASH, NUMBER, PERCENTAGE,
+                 DIMENSION, URL, UNICODE_RANGE, COLON, DELIMITER, BRACKET_OPEN,
+                 BRACKET_CLOSED, PAREN_OPEN, PAREN_CLOSED, BRACE_OPEN,
+                 BRACE_CLOSED, WHITESPACE, COMMENT, INCLUDES, DASHMATCH, OTHER,
+                 EOS}; 
 	
   Tokenizer(istream* in);
 		
