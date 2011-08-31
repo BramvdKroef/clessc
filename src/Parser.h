34ca0b23bec6208ba79b9574c45deaf5e360a565
Bram van der Kroef <bram@vanderkroef.net>
2011-08-31 08:15:44 -0500

    Added css2 spec to the comments.

--- src/Parser.h
+++ src/Parser.h
@@ -11,7 +11,26 @@ using namespace std;
 /**
  * Parses CSS code according to the syntax spec at
  * http://www.w3.org/TR/CSS2/syndata.html.
+ * Excerpt:
  * 
+ *   stylesheet  : [ CDO | CDC | S | statement ]*;
+ *   statement   : ruleset | at-rule;
+ *   at-rule     : ATKEYWORD S* any* [ block | ';' S* ];
+ *   block       : '{' S* [ any | block | ATKEYWORD S* | ';' S* ]* '}' S*;
+ *   ruleset     : selector? '{' S* declaration? [ ';' S* declaration? ]* '}' S*;
+ *   selector    : any+;
+ *   declaration : property S* ':' S* value;
+ *   property    : IDENT;
+ *   value       : [ any | block | ATKEYWORD S* ]+;
+ *   any         : [ IDENT | NUMBER | PERCENTAGE | DIMENSION | STRING
+ *                  | DELIM | URI | HASH | UNICODE-RANGE | INCLUDES
+ *                  | DASHMATCH | ':' | FUNCTION S* [any|unused]* ')'
+ *                  | '(' S* [any|unused]* ')' | '[' S* [any|unused]* ']'
+ *                 ] S*;
+ *   unused      : block | ATKEYWORD S* | ';' S* | CDO S* | CDC S*;
+ *
+ * The CDO ('<!--') and CDC ('-->') are not supported by the parser as
+ * they are not expected in a .css file.
  */
 
 class Parser{
