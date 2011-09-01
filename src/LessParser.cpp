0972faa6203acd3ab978f0f8ecd21511181c36f6
Bram van der Kroef <bram@vanderkroef.net>
2011-09-01 10:55:10 -0500

    Extended CssParser to support Less CSS code. Variables are supported now.

---
+++ src/LessParser.cpp
@@ -0,0 +1,170 @@
+#include "LessParser.h"
+
+bool LessParser::parseStatement(Stylesheet* stylesheet) {
+  Ruleset* ruleset = parseRuleset();
+  if (ruleset != NULL) {
+    stylesheet->addRuleset(ruleset);
+    return true;
+  }
+  
+  return parseAtRuleOrVariable(stylesheet);
+}
+
+bool LessParser::parseAtRuleOrVariable (Stylesheet* stylesheet) {
+  string* keyword = NULL;
+  vector<Token*>* rule;
+  AtRule* atrule = NULL;
+  
+  if (tokenizer->getTokenType() != Token::ATKEYWORD) 
+    return false;
+
+  keyword = new string(tokenizer->getToken()->str);
+  tokenizer->readNextToken();
+  skipWhitespace();
+
+  if (tokenizer->getTokenType() == Token::COLON) {
+    tokenizer->readNextToken();
+    skipWhitespace();
+    
+    rule = parseValue();
+    if (rule == NULL) {
+      throw new ParseException(tokenizer->getToken()->str,
+                               "value for variable");
+    }
+    if (tokenizer->getTokenType() != Token::DELIMITER) {
+      throw new ParseException(tokenizer->getToken()->str,
+                               "delimiter (';') at end of @-rule");
+    }
+    variables[*(keyword)] = rule;
+    tokenizer->readNextToken();
+    skipWhitespace();
+    
+  } else {
+    rule = new vector<Token*>();
+    while(parseAny(rule)) {};
+  
+    if (!parseBlock(rule)) {
+      if (tokenizer->getTokenType() != Token::DELIMITER) {
+        throw new ParseException(tokenizer->getToken()->str,
+                                 "delimiter (';') at end of @-rule");
+      }
+      tokenizer->readNextToken();
+      skipWhitespace();
+    }
+    atrule = new AtRule(keyword);
+    atrule->setRule(rule);
+    stylesheet->addAtRule(atrule);
+  }
+  return true;
+}
+
+Ruleset* LessParser::parseRuleset () {
+  Ruleset* ruleset = NULL;
+  Declaration* declaration = NULL;
+  vector<Token*>* selector = parseSelector();
+  
+  if (selector == NULL) {
+    if (tokenizer->getTokenType() != Token::BRACKET_OPEN) 
+      return NULL;
+  } else if (tokenizer->getTokenType() != Token::BRACKET_OPEN) {
+    throw new ParseException(tokenizer->getToken()->str,
+                             "a declaration block ('{...}')");
+  }
+  tokenizer->readNextToken();
+
+  ruleset = new Ruleset();
+  ruleset->setSelector(selector);
+  
+  skipWhitespace();
+  declaration = parseDeclaration();
+  if (declaration != NULL)
+    ruleset->addDeclaration(declaration);
+  
+  while (tokenizer->getTokenType() == Token::DELIMITER) {
+    tokenizer->readNextToken();
+    skipWhitespace();
+    declaration = parseDeclaration();
+    if (declaration != NULL)
+      ruleset->addDeclaration(declaration);
+  }
+  
+  if (tokenizer->getTokenType() != Token::BRACKET_CLOSED) {
+    throw new ParseException(tokenizer->getToken()->str,
+                             "end of declaration block ('}')");
+  } 
+  tokenizer->readNextToken();
+  skipWhitespace();
+  
+  return ruleset;
+}
+
+
+
+vector<Token*>* LessParser::parseValue () {
+  vector<Token*>* value = CssParser::parseValue();
+  if (value != NULL) {
+    vector<Token*>* newvalue = processValue(value);
+    delete value;
+    return newvalue;
+  } else
+    return value;
+}
+
+vector<Token*>* LessParser::processValue(vector<Token*>* value) {
+  vector<Token*>* newvalue = new vector<Token*>();
+  vector<Token*>::iterator it;
+  Token* token = NULL;
+  
+  for(it = value->begin(); it < value->end(); it++) {
+    token = *it;
+    
+    if (processVariable(token, newvalue)) {
+    } else if (processDeepVariable(token, (*(it + 1)), newvalue)) { 
+      it++;
+    } else
+      newvalue->push_back(token->clone());
+  }
+  return newvalue;
+}
+
+bool LessParser::processVariable (Token* token, vector<Token*>* newvalue) {
+  vector<Token*>* var;
+  vector<Token*>::iterator it;
+
+  if (token->type == Token::ATKEYWORD && variables.count(token->str)) {
+    var = variables[token->str];
+    for (it = var->begin(); it < var->end(); it++) 
+      newvalue->push_back((*it)->clone());
+
+    return true;
+  } else
+    return false;
+}
+
+bool LessParser::processDeepVariable (Token* token, Token* nexttoken,
+                                      vector<Token*>* newvalue) {
+  vector<Token*>* var;
+  vector<Token*>::iterator it;
+  string key("@");
+  
+  if (token->type == Token::OTHER &&
+      token->str == "@" &&
+      nexttoken->type == Token::ATKEYWORD &&
+      variables.count(nexttoken->str)) {
+      
+    var = variables[nexttoken->str];
+    if (var->size() == 1 && var->front()->type == Token::STRING) {
+      key.append(var->front()->
+                 str.substr(1, var->front()->str.size() - 2));
+      if (variables.count(key)) {
+        
+        var = variables[key];
+        for (it = var->begin(); it < var->end(); it++) 
+          newvalue->push_back((*it)->clone());
+
+        return true;
+      }
+    }
+  }
+  return false;
+}
---
+++ src/LessParser.h
@@ -0,0 +1,66 @@
+#ifndef __LessParser_h__
+#define __LessParser_h__
+
+#include "CssParser.h"
+#include "Stylesheet.h"
+#include "Token.h"
+#include <map>
+#include <vector>
+
+/**
+ * Extends the css spec with these parts:
+ * * Variables
+ *  at-rule: ATKEYWORD S* [ ':' value ';' S* | any* [ block | ';' S* ] ];
+ *  
+ * * Mixins
+ *  ruleset: selector? '{' S* [declaration | selector]?
+ *               [ ';' S* [declaration | selector]? ]* '}' S*;
+ *  
+ * * Parametric Mixins
+ *  ruleset: [selector parameters?]? '{' S* declaration?
+ *              [ ';' S* declaration? ]* '}' S*;
+ * parameters: '(' parameter? [ ',' S* parameter ]* ')'
+ * parameter: ATKEYWORD [ ':' S* value ]? S*
+ * 
+ * * Nested Rules
+ *  ruleset: [selector parameters]? '{' S* [ declaration | ruleset ]?
+ *              [ ';' S* [ declaration | ruleset ]? ]* '}' S*;
+ * 
+ * * Operations
+ *
+ *  value: value-old [ [  '+' | '-' | '*' | '/' ] value-old ]*
+ *  
+ */
+class LessParser: public CssParser {
+public:
+  LessParser(CssTokenizer* tokenizer): CssParser(tokenizer) {}
+  
+protected:
+  map<string, vector<Token*>*> variables;
+  vector<Ruleset*> mixins;
+
+  /**
+   * If an AtRule->getRule() starts with a COLON, add the variable to
+   * variables and don't add it to the Stylesheet.
+   * 
+   */
+  bool parseStatement(Stylesheet* stylesheet);
+  
+  /**
+   * If the first token for the rule is a COLON, parse a
+   * value. Otherwise parse the usual at-rule.
+   */
+  bool parseAtRuleOrVariable (Stylesheet* stylesheet);
+
+  Ruleset* parseRuleset ();
+
+  vector<Token*>* parseValue ();
+
+private:
+  vector<Token*>* processValue(vector<Token*>* value);
+  bool processVariable (Token* token, vector<Token*>* newvalue);
+  bool processDeepVariable (Token* token, Token* nexttoken,
+                            vector<Token*>* newvalue);
+};
+
+#endif
