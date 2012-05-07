816bb8857ce0ff5923de3eda22585bb14d98326f
Bram van der Kroef <bram@vanderkroef.net>
2012-05-07 12:23:01 -0500

    Added CssParser unit tests for @rules, rulesets and selectors.

--- src/tests/CssParser_test.cpp
+++ src/tests/CssParser_test.cpp
@@ -1,5 +1,54 @@
 #include "../CssParser.h"
 #include "gtest/gtest.h"
 
-TEST(CssParserTest, Values) {
+// At rules
+TEST(CssParserTest, AtRule) {
+  istringstream in("@import somefile;");
+    
+  CssTokenizer t(&in);
+  CssParser p(&t);
+  Stylesheet s;
+  AtRule* at;
+
+  p.parseStylesheet(&s);
+  ASSERT_EQ(1, s.getAtRules()->size());
+  
+  at = s.getAtRules()->at(0);
+  ASSERT_STREQ("@import", at->getKeyword()->c_str());
+  ASSERT_STREQ("somefile", at->getRule()->toString()->c_str());
 }
+
+// Rulesets
+TEST(CssParserTest, Ruleset) {
+  istringstream in("selector {key: value;}");
+    
+  CssTokenizer t(&in);
+  CssParser p(&t);
+  Stylesheet s;
+  Ruleset* set;
+  Declaration* d;
+
+  p.parseStylesheet(&s);
+  ASSERT_EQ(1, s.getRulesets()->size());
+  set = s.getRulesets()->at(0);
+  ASSERT_STREQ("selector", set->getSelector()->toString()->c_str());
+  ASSERT_EQ(1, set->getDeclarations()->size());
+
+  d = set->getDeclarations()->at(0);
+  ASSERT_STREQ("key", d->getProperty()->c_str());
+  ASSERT_STREQ("value", d->getValue()->toString()->c_str());
+}
+
+// selector without block
+TEST(CssParserTest, Selector) {
+  istringstream in("selector ");
+    
+  CssTokenizer t(&in);
+  CssParser p(&t);
+  Stylesheet s;
+
+  EXPECT_THROW(p.parseStylesheet(&s), ParseException*);
+}
+
+
+
