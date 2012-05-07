765b88c622d15dbc287fbcc421e731bd3fd3b395
Bram van der Kroef <bram@vanderkroef.net>
2012-05-07 13:29:57 -0500

    Added more test cases to the CssParser unit test.

--- src/tests/CssParser_test.cpp
+++ src/tests/CssParser_test.cpp
@@ -50,5 +50,73 @@ TEST(CssParserTest, Selector) {
   EXPECT_THROW(p.parseStylesheet(&s), ParseException*);
 }
 
+// ruleset without block end
+TEST(CssParserTest, RulesetNoEnding) {
+  istringstream in("selector {key: value");
+    
+  CssTokenizer t(&in);
+  CssParser p(&t);
+  Stylesheet s;
+
+  EXPECT_THROW(p.parseStylesheet(&s), ParseException*);
+}
+
+// declaration without value
+TEST(CssParserTest, DeclarationNoValue) {
+  istringstream in("selector {key: }");
+    
+  CssTokenizer t(&in);
+  CssParser p(&t);
+  Stylesheet s;
+
+  EXPECT_THROW(p.parseStylesheet(&s), ParseException*);
+}
+
+// declaration without colon
+TEST(CssParserTest, DeclarationNoColon) {
+  istringstream in("selector {key value}");
+    
+  CssTokenizer t(&in);
+  CssParser p(&t);
+  Stylesheet s;
+
+  EXPECT_THROW(p.parseStylesheet(&s), ParseException*);
+}
+
+// @keyword in value
+TEST(CssParserTest, AtValue) {
+  istringstream in("selector {key: @value}");
+    
+  CssTokenizer t(&in);
+  CssParser p(&t);
+  Stylesheet s;
+  Declaration* d;
+
+  p.parseStylesheet(&s);
+  ASSERT_EQ(1, s.getRulesets()->size());
+  ASSERT_EQ(1, s.getRulesets()->at(0)->getDeclarations()->size());
+
+  d = s.getRulesets()->at(0)->getDeclarations()->at(0);
+  ASSERT_STREQ("key", d->getProperty()->c_str());
+  ASSERT_STREQ("@value", d->getValue()->toString()->c_str());
+}
+
+// block in value
+TEST(CssParserTest, BlockValue) {
+  istringstream in("selector {key: {value}}");
+    
+  CssTokenizer t(&in);
+  CssParser p(&t);
+  Stylesheet s;
+  Declaration* d;
+
+  p.parseStylesheet(&s);
+  ASSERT_EQ(1, s.getRulesets()->size());
+  ASSERT_EQ(1, s.getRulesets()->at(0)->getDeclarations()->size());
+
+  d = s.getRulesets()->at(0)->getDeclarations()->at(0);
+  ASSERT_STREQ("key", d->getProperty()->c_str());
+  ASSERT_STREQ("{value}", d->getValue()->toString()->c_str());
+}
 
 
