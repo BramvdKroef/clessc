935379ce0b0711202944d5b97624dd148420a044
Bram van der Kroef <bram@vanderkroef.net>
2017-06-29 08:21:27 -0500

    Fixed CssParser tests.

--- tests/CssParser_test.cpp
+++ tests/CssParser_test.cpp
@@ -19,125 +19,126 @@
  * Author: Bram van der Kroef <bram@vanderkroef.net>
  */
 
-#include "CssParser.h"
+#include "css/CssParser.h"
 #include "gtest/gtest.h"
 
-// At rules
-TEST(CssParserTest, AtRule) {
-  istringstream in("@import somefile;");
+class CssParserTest : public ::testing::Test {
+public:
+  istringstream* in;
     
-  CssTokenizer t(&in);
-  CssParser p(&t);
+  CssTokenizer* t;
+  CssParser* p;
+
+  virtual void SetUp() {
+    in = new istringstream(" ");
+    t = new CssTokenizer(*in, "test");
+    p = new CssParser(*t);
+  }
+  virtual void TearDown() {
+    delete p;
+    delete t;
+    delete in;
+  }
+  
+};
+  
+// At rules
+TEST_F(CssParserTest, AtRule) {
+  in->str("@import somefile;");
   Stylesheet s;
   AtRule* at;
-
-  p.parseStylesheet(&s);
-  ASSERT_EQ(1, s.getAtRules()->size());
   
-  at = s.getAtRules()->at(0);
-  ASSERT_STREQ("@import", at->getKeyword()->c_str());
-  ASSERT_STREQ("somefile", at->getRule()->toString()->c_str());
+  p->parseStylesheet(s);
+  ASSERT_EQ(1, s.getAtRules().size());
+  
+  at = s.getAtRules().front();
+  ASSERT_STREQ("@import", at->getKeyword().c_str());
+  ASSERT_STREQ("somefile", at->getRule().toString().c_str());
 }
 
 // Rulesets
-TEST(CssParserTest, Ruleset) {
-  istringstream in("selector {key: value;}");
-    
-  CssTokenizer t(&in);
-  CssParser p(&t);
+TEST_F(CssParserTest, Ruleset) {
+  in->str("selector {key: value;}");
+  
   Stylesheet s;
   Ruleset* set;
   Declaration* d;
 
-  p.parseStylesheet(&s);
-  ASSERT_EQ(1, s.getRulesets()->size());
-  set = s.getRulesets()->at(0);
-  ASSERT_STREQ("selector", set->getSelector()->toString()->c_str());
-  ASSERT_EQ(1, set->getDeclarations()->size());
+  p->parseStylesheet(s);
+  ASSERT_EQ(1, s.getRulesets().size());
+  set = s.getRulesets().front();
+  ASSERT_STREQ("selector", set->getSelector().toString().c_str());
+  ASSERT_EQ(1, set->getDeclarations().size());
 
-  d = set->getDeclarations()->at(0);
-  ASSERT_STREQ("key", d->getProperty()->c_str());
-  ASSERT_STREQ("value", d->getValue()->toString()->c_str());
+  d = set->getDeclarations().front();
+  ASSERT_STREQ("key", d->getProperty().c_str());
+  ASSERT_STREQ("value", d->getValue().toString().c_str());
 }
 
 // selector without block
-TEST(CssParserTest, Selector) {
-  istringstream in("selector ");
-    
-  CssTokenizer t(&in);
-  CssParser p(&t);
+TEST_F(CssParserTest, Selector) {
+  in->str("selector ");
+
   Stylesheet s;
 
-  EXPECT_THROW(p.parseStylesheet(&s), ParseException*);
+  EXPECT_THROW(p->parseStylesheet(s), ParseException*);
 }
 
 // ruleset without block end
-TEST(CssParserTest, RulesetNoEnding) {
-  istringstream in("selector {key: value");
+TEST_F(CssParserTest, RulesetNoEnding) {
+  in->str("selector {key: value");
     
-  CssTokenizer t(&in);
-  CssParser p(&t);
   Stylesheet s;
 
-  EXPECT_THROW(p.parseStylesheet(&s), ParseException*);
+  EXPECT_THROW(p->parseStylesheet(s), ParseException*);
 }
 
 // declaration without value
-TEST(CssParserTest, DeclarationNoValue) {
-  istringstream in("selector {key: }");
-    
-  CssTokenizer t(&in);
-  CssParser p(&t);
+TEST_F(CssParserTest, DeclarationNoValue) {
+  in->str("selector {key: }");
   Stylesheet s;
 
-  EXPECT_THROW(p.parseStylesheet(&s), ParseException*);
+  EXPECT_THROW(p->parseStylesheet(s), ParseException*);
 }
 
 // declaration without colon
-TEST(CssParserTest, DeclarationNoColon) {
-  istringstream in("selector {key value}");
-    
-  CssTokenizer t(&in);
-  CssParser p(&t);
+TEST_F(CssParserTest, DeclarationNoColon) {
+  in->str("selector {key value}");
   Stylesheet s;
 
-  EXPECT_THROW(p.parseStylesheet(&s), ParseException*);
+  EXPECT_THROW(p->parseStylesheet(s), ParseException*);
 }
 
 // @keyword in value
-TEST(CssParserTest, AtValue) {
-  istringstream in("selector {key: @value}");
+TEST_F(CssParserTest, AtValue) {
+  in->str("selector {key: @value}");
     
-  CssTokenizer t(&in);
-  CssParser p(&t);
   Stylesheet s;
   Declaration* d;
 
-  p.parseStylesheet(&s);
-  ASSERT_EQ(1, s.getRulesets()->size());
-  ASSERT_EQ(1, s.getRulesets()->at(0)->getDeclarations()->size());
+  p->parseStylesheet(s);
+  ASSERT_EQ(1, s.getRulesets().size());
+  ASSERT_EQ(1, s.getRulesets().front()->getDeclarations().size());
 
-  d = s.getRulesets()->at(0)->getDeclarations()->at(0);
-  ASSERT_STREQ("key", d->getProperty()->c_str());
-  ASSERT_STREQ("@value", d->getValue()->toString()->c_str());
+  d = s.getRulesets().front()->getDeclarations().front();
+  ASSERT_STREQ("key", d->getProperty().c_str());
+  ASSERT_STREQ("@value", d->getValue().toString().c_str());
 }
 
 // block in value
-TEST(CssParserTest, BlockValue) {
-  istringstream in("selector {key: {value}}");
+TEST_F(CssParserTest, BlockValue) {
+  in->str("selector {key: {value}}");
     
-  CssTokenizer t(&in);
-  CssParser p(&t);
   Stylesheet s;
   Declaration* d;
 
-  p.parseStylesheet(&s);
-  ASSERT_EQ(1, s.getRulesets()->size());
-  ASSERT_EQ(1, s.getRulesets()->at(0)->getDeclarations()->size());
+  p->parseStylesheet(s);
+  ASSERT_EQ(1, s.getRulesets().size());
+  ASSERT_EQ(1, s.getRulesets().front()->getDeclarations().size());
 
-  d = s.getRulesets()->at(0)->getDeclarations()->at(0);
-  ASSERT_STREQ("key", d->getProperty()->c_str());
-  ASSERT_STREQ("{value}", d->getValue()->toString()->c_str());
+  d = s.getRulesets().front()->getDeclarations().front();
+  ASSERT_STREQ("key", d->getProperty().c_str());
+  ASSERT_STREQ("{value}", d->getValue().toString().c_str());
 }
 
 
